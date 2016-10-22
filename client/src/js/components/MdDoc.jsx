899e1c32709b007792fae232c3ddeefc7f4f0437
Dirk Roorda <dirk.roorda@dans.knaw.nl>
2016-10-22 13:18:54 +0200

    update

--- client/src/js/components/MdDoc.jsx
+++ client/src/js/components/MdDoc.jsx
@@ -1,14 +1,24 @@
 import React, {Component, PropTypes} from 'react'
 import { render } from 'react-dom'
 import Markdown from 'react-markdown'
+import { Link } from 'react-router'
 import mdText from '../helpers/mdText.js'
+
+const RouterLink = (props) => (
+  props.href.match(/^(https?:)?\/\//)
+    ? <a href={props.href}>{props.children}</a>
+    : <Link to={props.href}>{props.children}</Link>
+)
 
 const MdDoc = (props) => {
   const textSource = props.docName;
   const text = mdText[textSource];
   return (
     <div style={{paddingLeft: '0.5em'}}>
-      <Markdown source={text}/>
+      <Markdown
+        source={text}
+        renderers={{Link: RouterLink}}
+      />
     </div>
   )
 }
