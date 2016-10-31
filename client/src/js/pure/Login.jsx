f3dd9299279baabece40647322e14d7c826142fe
Dirk Roorda <dirk.roorda@dans.knaw.nl>
2016-10-31 21:44:53 +0100

    update

--- client/src/js/pure/Login.jsx
+++ client/src/js/pure/Login.jsx
@@ -1,17 +1,12 @@
 import React, {PropTypes} from 'react'
 
 const Login = ({ user, login, logout }) => {
-  const access = 'loggedin';
-  const label = {
-    loggedin: 'logged in',
-  }
   return (
     <span style={{float: 'right', fontSize: 'small'}}>
       { user && Object.keys(user).length > 0 ? (
       <span style={{color: '#333333'}}>
         <strong className="fa fa-user">{user.eppn}</strong>
-        <span className="fa fa-hashtag"/>{` ${user.authority} `}
-          <span>{label[access]}</span>
+        <span className="fa fa-hashtag"/>{user.authority}{' '}
           <a href="/logout" className="control fa fa-user-times">{' logout'}</a>
           <a href="/slogout" className="control fa fa-users">{' quit '}</a>
         </span>
