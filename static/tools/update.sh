0d6a1faa7ae43f28b9d4553732449ae997dc7a46
Dirk Roorda <dirk.roorda@dans.knaw.nl>
2018-07-17 12:37:07 +0200

    ship: update script

--- static/tools/update.sh
+++ static/tools/update.sh
@@ -20,15 +20,25 @@ if [ "$HOSTNAME" == "tclarin11.dans.knaw.nl" ]; then
 fi
 
 if [ $ON_CLARIN ]; then
+    echo "UPDATE.SH: stopping the web server"
     service httpd stop
+    echo "UPDATE.SH: web server stopped"
 fi
 
 cd $ADIR/$APP
+echo "UPDATE.SH: Pulling the dariah app from GitHub"
 git pull origin master
+echo "UPDATE.SH: Dariah app pulled from GitHub"
+echo "UPDATE.SH: Enabling rh-python36"
 scl enable rh-python36 bash
+echo "UPDATE.SH: rh-python36 enabled"
+echo "UPDATE.SH: Python byte-compiling server code"
 python3 -m compileall server
+echo "UPDATE.SH: Server code python byte-compiled"
 
 if [ $ON_CLARIN ]; then
+    echo "UPDATE.SH: starting the web server"
     service httpd start
+    echo "UPDATE.SH: web server started"
 fi
 
